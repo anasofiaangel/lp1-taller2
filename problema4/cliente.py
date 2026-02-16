47dc3f2ca1d9004d90187b72c805b23d1ff97d8a
ana sofia <anasofiangel05@gmail.com>
2026-02-16 18:24:08 -0500

    problema4

--- problema4/cliente.py
+++ problema4/cliente.py
@@ -6,24 +6,41 @@ Objetivo: Crear un cliente HTTP que realice una petición GET a un servidor web 
 
 import http.client
 
-# TODO: Definir la dirección y puerto del servidor HTTP
+#  Definir la dirección y puerto del servidor HTTP
+host = "localhost"   # o "0.0.0.0" para aceptar conexiones externas
+port = 8080          # puedes cambiarlo al puerto que prefieras
 
-# TODO: Crear una conexión HTTP con el servidor
+# Crear servidor
+server = HTTPServer((host, port), SimpleHTTPRequestHandler)
+
+print(f"Servidor iniciado en http://{host}:{port}")
+server.serve_forever()
+
+#  Crear una conexión HTTP con el servidor
+conn = http.client.HTTPConnection(host, port)
 # HTTPConnection permite establecer conexiones HTTP con servidores
 
-# TODO: Realizar una petición GET al path raíz ('/')
+#  Realizar una petición GET al path raíz ('/')
 # request() envía la petición HTTP al servidor
 # Primer parámetro: método HTTP (GET, POST, etc.)
 # Segundo parámetro: path del recurso solicitado
+conn.request("GET", "/")
 
-# TODO: Obtener la respuesta del servidor
+# Obtener la respuesta del servidor
 # getresponse() devuelve un objeto HTTPResponse con los datos de la respuesta
+response = conn.getresponse()
 
-# TODO: Leer el contenido de la respuesta
+#  Leer el contenido de la respuesta
 # read() devuelve el cuerpo de la respuesta en bytes
+data = response.read()
 
-# TODO: Decodificar los datos de bytes a string e imprimirlos
+#  Decodificar los datos de bytes a string e imprimirlos
 # decode() convierte los bytes a string usando UTF-8 por defecto
+print("Código de estado:", response.status)
+print("Razón:", response.reason)
+print("Contenido:", data.decode())
 
-# TODO: Cerrar la conexión con el servidor
+#  Cerrar la conexión con el servidor
+conn.close()
+
 
